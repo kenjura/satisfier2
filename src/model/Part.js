3b9ce3a0a0cb0e9c1f1efcfdd604edb3cc54eb80
Kenjura <github@kenjura.com>
2022-08-25 21:54:21 -0700

    major cleanup to recipe data

--- src/model/Part.js
+++ src/model/Part.js
@@ -9,6 +9,8 @@ export interface IPart {
 export default class Part {
   name: string;
   static findAll: () => Array<Part>;
+  static stub: (string) => ?Part;
+  static stubRequired: (string) => Part;
 
   constructor(props: IPart) {
     this.name = props.name;
@@ -16,11 +18,25 @@ export default class Part {
 }
 
 Part.findAll = moize(findAll);
+Part.stub = moize(stub);
+Part.stubRequired = moize(stub);
 
 function findAll(): Array<Part> {
   const recipes = Recipe.findAll();
   let partNames = new Set<string>();
   recipes.forEach((recipe) => partNames.add(recipe.outputPart.name));
   return Array.from(partNames).map((partName) => new Part({ name: partName }));
-};
+}
 
+function stub(name: ?string): ?Part {
+  if (name === null || name === undefined) return null;
+  return new Part({ name });
+}
+
+function stubRequired(name: string): ?Part {
+  if (name === null || name === undefined) {
+    // this data is loaded dynamically; Flow won't catch these errors
+    throw new Error("Part > stubRequired > name must be supplied.");
+  }
+  return new Part({ name });
+}
--- src/model/Recipe.js
+++ src/model/Recipe.js
@@ -9,16 +9,16 @@ export interface IRecipe {
   name: string;
   outputPart: Part;
   outputQuantity: number;
-  inputPart1?: Part;
-  inputQuantity1?: number;
-  inputPart2?: Part;
-  inputQuantity2?: number;
-  inputPart3?: Part;
-  inputQuantity3?: number;
-  inputPart4?: Part;
-  inputQuantity4?: number;
-  byproductPart?: Part;
-  byproductQuantity?: number;
+  inputPart1: ?Part;
+  inputQuantity1: ?number;
+  inputPart2: ?Part;
+  inputQuantity2: ?number;
+  inputPart3: ?Part;
+  inputQuantity3: ?number;
+  inputPart4: ?Part;
+  inputQuantity4: ?number;
+  byproductPart: ?Part;
+  byproductQuantity: ?number;
   stage: number;
   building?: string;
   isAlternate: boolean;
@@ -65,23 +65,23 @@ function findAll() {
     .map(
       (rawRecipe) =>
         new Recipe({
-          name: rawRecipe.Recipe,
-          outputPart: new Part({ name: rawRecipe["Output"] }),
-          outputQuantity: Number(rawRecipe["Output qty/min"]),
-          inputPart1: new Part({ name: rawRecipe["Item 1"] }),
-          inputQuantity1: Number(rawRecipe["Q1"]),
-          inputPart2: new Part({ name: rawRecipe["Item 2"] }),
-          inputQuantity2: Number(rawRecipe["Q2"]),
-          inputPart3: new Part({ name: rawRecipe["Item 3"] }),
-          inputQuantity3: Number(rawRecipe["Q3"]),
-          inputPart4: new Part({ name: rawRecipe["Item 4"] }),
-          inputQuantity4: Number(rawRecipe["Q4"]),
-          byproductPart: new Part({ name: rawRecipe["Byproduct"] }),
-          byproductQuantity: Number(rawRecipe["QB"]),
-          isAlternate: rawRecipe["Alternate"] === "yes",
-          altScore: Number(rawRecipe["Alt Score"] || 0),
-          stage: Number(rawRecipe["Stage"]),
-          building: rawRecipe["Building"],
+          name: rawRecipe.recipe,
+          outputPart: Part.stubRequired(rawRecipe.output),
+          outputQuantity: Number(rawRecipe.outputQtyPerMin),
+          inputPart1: Part.stub(rawRecipe.item1),
+          inputQuantity1: Number(rawRecipe.itemQty1),
+          inputPart2: Part.stub(rawRecipe.item2),
+          inputQuantity2: Number(rawRecipe.itemQty2),
+          inputPart3: Part.stub(rawRecipe.item3),
+          inputQuantity3: Number(rawRecipe.itemQty3),
+          inputPart4: Part.stub(rawRecipe.item4),
+          inputQuantity4: Number(rawRecipe.itemQty4),
+          byproductPart: Part.stub(rawRecipe.byproduct),
+          byproductQuantity: Number(rawRecipe.byproductQty),
+          isAlternate: rawRecipe.alternate === "yes",
+          altScore: Number(rawRecipe.altScore || 0),
+          stage: Number(rawRecipe.stage),
+          building: rawRecipe.building,
         })
     );
   return recipes;
